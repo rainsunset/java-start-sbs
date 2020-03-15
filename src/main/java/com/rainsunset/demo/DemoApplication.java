6e920af1e03378f57b2e6b58030b316dc5ddf09b
rainsunset <15620608789@163.com>
2020-03-15 23:27:59 +0800

    修改说明

--- src/main/java/com/rainsunset/demo/DemoApplication.java
+++ src/main/java/com/rainsunset/demo/DemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@MapperScan("com.rainsunset.*.dal.mapper")
+@MapperScan("com.rainsunset.demo.dao.mapper")
 @EnableTransactionManagement
 public class DemoApplication {
 	public static void main(String[] args) {
